634de7060f5f760f3124ee8f7dab352eb8dbfcba
daiya122815 <daiya122815@gmail.com>
2024-10-21 10:14:02 +0900

    オブジェクトの配列の作成

--- src/App.jsx
+++ src/App.jsx
@@ -25,17 +25,48 @@
 // }
 
 export default function App() {
+  // const obj = {
+  //   "src": "src",
+  //   "alt": "alt"
+  // };
+
+  // const objArray = [
+  //   obj.key = ,
+  // ];
+
+  const array = [
+    { "images/pic1.jpg": "Closeup of a human eye" },
+    { "images/pic2.jpg": "Rock that looks like a wave" },
+    { "images/pic3.jpg": "Purple and white pansies" },
+    { "images/pic4.jpg": "Section of wall from a pharoah's tomb" },
+    { "images/pic5.jpg": "Large moth on a leaf" }
+  ];
+
+  function handleClick(event) {
+    event.preventDefault();
+    alert("hello");
+  }
   return (
     <>
       <h1>Image gallery example</h1>
       <div className="full-img">
         <img
+          onClick={handleClick}
           className="displayed-img"
           src="images/pic1.jpg"
           alt="Closeup of a human eye"
         />
+        {/* {array.map((src, alt) => {
+          return (
+            <img
+              onClick={(handleClick)}
+              key={alt}
+              src={src}
+            />
+          );
+        })} */}
         <div className="overlay"></div>
-        <button className="dark">Darken</button>
+        <button onClick={handleClick} className="dark">Darken</button>
       </div>
       <div className="thumb-bar"></div>
     </>
